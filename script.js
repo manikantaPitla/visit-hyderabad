5ac3ea7ac4cefef320d3db13a5985ac3ba73469d
manikantaPitla <127218186+manikantaPitla@users.noreply.github.com>
2023-07-30 19:03:52 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,8 @@
+function navLinks() {
+    let navLinks = document.getElementById("navLinks");
+    let backBlur = document.getElementById("backBlur");
+
+    navLinks.classList.toggle("nav-links-open")
+    backBlur.classList.toggle("back-blur")
+        
+}
